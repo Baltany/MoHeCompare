e3b65cc3b04b9c66652c8fdea0e60c4309455c18
Balbino Moyano López <bmoylop0903@g.educaand.es>
2024-06-30 11:41:29 +0200

    Perfilando las clases modelo

--- dev/src/main/java/dev/mohecompare/dev/modelo/ProductoSuperMercado.java
+++ dev/src/main/java/dev/mohecompare/dev/modelo/ProductoSuperMercado.java
@@ -1,9 +1,12 @@
 package dev.mohecompare.dev.modelo;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +19,15 @@ public class ProductoSuperMercado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    private Producto producto;
     
+    @ManyToOne
+    private SuperMercado superMercado;
+
+    private Float precio;
+    
+
+
     
 }
